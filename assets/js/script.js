ea22ab089ebf60d682fc79b84cc253ee63b8d3a8
mikwitz <mikaylahowitz@gmail.com>
2021-11-08 15:59:01 -0700

    refactor the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,25 @@
 console.dir(window.document);
 
-var buttonEl = document.querySelector("#save-task");
 var formEl = document.querySelector("#task-form")
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput =  document.querySelector("select[name='task-type']").value;
-    //create a list item
+
+    //package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    // send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function(taskDataObj) {
     var listItemEl =document.createElement("li");
     listItemEl.className = "task-item";
     
@@ -18,15 +28,13 @@ var createTaskHandler = function(event) {
     //give it a class name for css styles
     taskInfoEl.className = "task-info";
     //add html content to div appended data to li
-    taskInfoEl.innerHTML = "<h3 class = 'task-name'>" + taskNameInput + "</h3><span class= 'task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class = 'task-name'>" + taskDataObj.name + "</h3><span class= 'task-type'>" + taskDataObj.type + "</span>";
 
     listItemEl.appendChild(taskInfoEl);
     //appended the entire li to the parent ul 
     tasksToDoEl.appendChild(listItemEl);
+}
 
-    console.dir(listItemEl);
-};
-
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
 
 
